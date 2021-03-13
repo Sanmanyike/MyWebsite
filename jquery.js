94b3f863d24031e25e33cace8489b742f2a79032
Sanmanyike <terrencemanyike@gmail.com>
2021-03-13 21:47:41 +0300

    Update on jquery

--- jquery.js
+++ jquery.js
@@ -1,6 +1,15 @@
+//Runs after the page has loaded
 $(document).ready(function(){
-    function myAlert(){
+    //Show alert box and profile picture
+    $(window).on("load",function(){
         $(".alert").show(1000);
-    }
-    $(window).on("load",myAlert());
+    });
+    $(window).on("load",function(){
+        $(".profilepic").show(2000);
+    });
+    //Show about cards and more
+    var x = window.scrollTo(0,100);
+    $(".about-u").on("click",function(){
+        
+    })
 });
