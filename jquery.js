bf76aeb9c54f2453a89e29539f3ea9c1df38e981
Sanmanyike <terrencemanyike@gmail.com>
2021-03-15 22:32:05 +0300

    Update commit

--- jquery.js
+++ jquery.js
@@ -15,4 +15,44 @@ $(document).ready(function(){
             $(".about-card").show(1000);
         });
     }
+    //Skillset bar loading
+    $(".skills").hover(function(){
+        $(".html-bar").animate({
+            width: "80%"
+        },1000);
+        $(".css-bar").animate({
+            width: "68%"
+        },1100);
+        $(".bootstrap-bar").animate({
+            width: "60%"
+        },1200);
+        $(".javascript-bar").animate({
+            width: "55%"
+        },1300);
+        $(".jquery-bar").animate({
+            width: "50%"
+        },1400);
+        $(".php-bar").animate({
+            width: "75%"
+        },1500);
+        $(".wordpress-bar").animate({
+            width: "0%"
+        },1600);
+        $(".xml-bar").animate({
+            width: "0%"
+        },1700);
+        $(".json-bar").animate({
+            width: "0%"
+        },1800);
+        $(".restapi-bar").animate({
+            width: "0%"
+        },1900);
+        $(".vue-bar").animate({
+            width: "0%"
+        },2000);
+        $(".c-bar").animate({
+            width: "10%"
+        },2100);
+
+    });
 });
